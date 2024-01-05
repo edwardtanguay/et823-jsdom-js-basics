310e96b42b4db8c53d24b32d54e0eed3258d2ebb
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 16:14:31 +0100

    finished

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,8 +1,32 @@
-export const Ball = (backgroundColor = "brown", borderSize = 4, text = "") => {
+export const Ball = (idCode, backgroundColor = "brown", borderSize = 4, text = "") => {
 
 	const ballSize = 9;
 	const boxShadow = 20;
 	let textColor = 'black';
+
+	function getRandomNumber() {
+		const randomDecimal = Math.random();
+		const randomNumber = Math.floor(randomDecimal * 5) + 1;
+		return randomNumber;
+	}
+
+
+	setTimeout(() => {
+
+		const ballElem = document.querySelector('.ball' + idCode);
+		// ballElem.style.width = '20rem';
+
+		let count = 10;
+		setInterval(() => {
+			const rand = getRandomNumber();
+			if (rand === 1) {
+				count++;
+			}
+			ballElem.style.width = `${count}rem`;
+			ballElem.style.height = `${count}rem`;
+		}, 100);
+	}, 0);
+
 
 	// if (backgroundColor === 'black' || backgroundColor === '#111') {
 	if (['black', '#111', '#222'].includes(backgroundColor)) {
@@ -10,7 +34,7 @@ export const Ball = (backgroundColor = "brown", borderSize = 4, text = "") => {
 	}
 
 	return /*html*/ `
-<div class="ball flex items-center justify-center 
+<div class="ball${idCode} ballMain flex items-center justify-center 
 " style=" color:${textColor}; border: ${borderSize}px solid white; box-shadow: ${boxShadow}px ${boxShadow}px ${boxShadow}px black; background-color: ${backgroundColor}; width: ${ballSize}rem; height: ${ballSize}rem">${text}</div>
 
 	`;
--- src/main.js
+++ src/main.js
@@ -9,21 +9,21 @@ document.querySelector('#app').innerHTML = /*html*/ `
 <h2 class="text-3xl mb-4">Title: ${title}</h2>
 
 <div class="flex gap-4 flex-wrap">
-	${Ball('blue', 8, "42")}
-	${Ball('black', 4, "that's not black")}
-	${Ball('purple')}
-	${Ball('yellow')}
-	${Ball()}
-	${Ball('#111')}
-	${Ball('#222')}
-	${Ball('green')}
-	${Ball('skyblue')}
-	${Ball()}
-	${Ball('orange', 1, "weekend")}
+	${Ball('001', 'black', 4, "that's not black")}
+	${Ball('002', 'blue', 8, "42")}
+	${Ball('003', 'purple')}
+	${Ball('004', 'yellow')}
+	${Ball('005',)}
+	${Ball('006', '#111')}
+	${Ball('007', '#222')}
+	${Ball('008', 'green')}
+	${Ball('009', 'skyblue')}
+	${Ball('010',)}
+	${Ball('011', 'orange', 1, "weekend")}
 </div>
-<section  class="border border-slate-600 mt-12 p-4 rounded">
+<!-- <section  class="border border-slate-600 mt-12 p-4 rounded">
 	${KidsNameTags()}
-</section>
+</section> -->
 
 `
 
