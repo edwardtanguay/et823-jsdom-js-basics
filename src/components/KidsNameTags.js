8af515693dfe103900dd1ee7b59936450f457b85
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 15:43:35 +0100

    sort

--- src/components/KidsNameTags.js
+++ src/components/KidsNameTags.js
@@ -2,6 +2,9 @@ import { Ball } from "./Ball";
 import kids from '../data/kids.json';
 
 export const KidsNameTags = () => {
+
+	kids.sort((a, b) => a.text > b.text ? 1 : -1);
+
 	return /*html*/ `
 	<div class="flex gap-4 flex-wrap">
 	${kids.map(kid => {
