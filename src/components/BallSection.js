96579c54fa45268beb2398672a558a6c536137a8
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 15:37:55 +0100

    have data

--- src/components/BallSection.js
+++ src/components/BallSection.js
@@ -1,33 +1,11 @@
 import { Ball } from "./Ball";
-
-const balls = [
-	{
-		color: "yellow",
-		borderSize: 8,
-		text: "Hans"
-	},
-	{
-		color: "orange",
-		borderSize: 8,
-		text: "Angela"
-	},
-	{
-		color: "blue",
-		borderSize: 8,
-		text: "Roger"
-	},
-	{
-		color: "black",
-		borderSize: 8,
-		text: "Patricia"
-	}
-];
+import kids from '../data/kids.json';
 
 export const BallSection = () => {
 	return /*html*/ `
 	<div class="flex gap-4 flex-wrap">
-	${balls.map(ball => {
-		return `${Ball(ball.color, ball.borderSize, ball.text)}`;
+	${kids.map(kid => {
+		return `${Ball(kid.color, kid.borderSize, kid.text)}`;
 	}).join('')}
 	</div>
 	`;
