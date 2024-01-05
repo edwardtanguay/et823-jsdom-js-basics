980250647b9a7fc681a10ef5a68f8839a956f3d5
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 15:02:38 +0100

    tailwind solution for color text

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,11 +1,12 @@
-export const Ball = (color = "brown", borderSize =4) => {
+export const Ball = (color = "brown", borderSize = 4) => {
 
 	const ballSize = 9;
-	// const boxShadow = 5
-	
+	const boxShadow = 20;
+
 
 	return /*html*/ `
-<div class="ball" style=" border: ${borderSize}px solid white; box-shadow: 5px 5px 5px black; background-color: ${color}; width: ${ballSize}rem; height: ${ballSize}rem"></div>
+<div class="ball flex items-center justify-center text-[cyan]
+" style=" border: ${borderSize}px solid white; box-shadow: ${boxShadow}px ${boxShadow}px ${boxShadow}px black; background-color: ${color}; width: ${ballSize}rem; height: ${ballSize}rem">Test</div>
 
 	`;
 };
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ document.querySelector('#app').innerHTML = /*html*/ `
 <h2 class="text-3xl mb-4">Title: ${title}</h2>
 
 <div class="flex gap-4 flex-wrap">
-${Ball('blue',8)}
+${Ball('blue',8,"42")}
 ${Ball('black')}
 ${Ball('purple')}
 ${Ball('yellow')}
