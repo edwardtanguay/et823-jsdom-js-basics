55ee5a42357956d1efa406bd032aa6a67f09acd1
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 14:15:23 +0100

    white border

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -3,7 +3,7 @@ export const Ball = (color = "brown") => {
 	const ballSize = 9;
 
 	return /*html*/ `
-<div class="ball" style="background-color: ${color}; width: ${ballSize}rem; height: ${ballSize}rem"></div>
+<div class="ball" style=" border: 2px solid white; background-color: ${color}; width: ${ballSize}rem; height: ${ballSize}rem"></div>
 
 	`;
 };
